76cf682a4fef48fe0fb8a8b23125020343afecc7
Alaa Eddin Albarghoth <alaaeddin.albarghoth@appcent.mobi>
2022-05-16 15:54:47 +0300

    Create the new design of the card

--- lib/main.dart
+++ lib/main.dart
@@ -19,29 +19,72 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.white,
-                child: const Text('Container 1'),
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/alaa.jpg'),
               ),
-              const SizedBox(
-                width: 10.0,
+              const Text(
+                'Alaa Eddin Albarghoth',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                height: 50.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: const Text('Container 2'),
+              Text(
+                'Mobile Developer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                height: 50.0,
-                width: 100.0,
-                color: Colors.red,
-                child: const Text('Container 3'),
+              SizedBox(
+                width: 250.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+90 553 956 5750',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 15.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'eng.alaaeddin.albarghoth@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 15.0),
+                  ),
+                ),
               ),
             ],
           ),
